39be8c8315aad053b08ba57891eb44990266ac7b
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-10-01 12:34:13 +0530

    Create que2.c

---
+++ Experiment1/que2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Abhishek Bansal\n");
+    printf("123, ABC Street\n");
+    printf("Dehradun, Uttarakhand\n");
+    printf("India\n");
+    return 0;
+}
