5b08d216adc8d67572eefc34b4d786e35345b300
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-10-07 00:39:30 +0530

    Update que5.c

--- Experiment6/que5.c
+++ Experiment6/que5.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+// Function to reverse a string
+void REVERSE(char str[]) {
+    int i, j;
+    char temp;
+    int len = strlen(str);
+
+    // Swap characters from both ends moving toward the center
+    for (i = 0, j = len - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = '\0';
+
+    printf("\n-------------------------------------------\n");
+    printf("      Original String: %s\n", str);
+
+    REVERSE(str);  // Function call
+
+    printf("      Reversed String: %s\n", str);
+    printf("-------------------------------------------\n");
+
+    return 0;
+}
 
